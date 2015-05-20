9e63bfca8ce6c7c4bb64cf8effd8a7da13e84e9c
Rowly <mail.rowlands@gmail.com>
2015-05-20 10:50:50 +0100

    New ruby script to handle TX thumb screenshot
    As I was having real troubles getting python-WebDriver to work on the
    Rpi, I tried ruby-webdriver. This worked. Made a new capture.rb and edit
    to test_video.py to call capture.rb with Popen

---
+++ src/root/nested/utilities/capture.rb
@@ -0,0 +1,29 @@
+require 'selenium-webdriver'
+require 'headless'
+
+target = ARGV[0]
+
+headless = Headless.new
+headless.start
+
+driver = Selenium::Webriver.for :firefox
+wait = Selenium::WebDriver::Wait.new(:timeout => 60)
+
+driver.navigate.to target
+
+def wait_for_clickable_element(locator)
+  element = wait.until { driver.find_element(locator) }
+  wait.until { element.displayed? }
+  wait.until { element.enabled? }
+  return element
+end
+
+element = wait_for_clickable_element(:css => "#refresh_thumbnail")
+element.click
+
+element = wait_for_clickable_element(:css => "#right > div > a:nth-child(5)")
+element.click
+
+driver.save_screenshot("../imgs/capture.png")
+
+headless.destroy
--- src/root/nested/utilities/test_video.py
+++ src/root/nested/utilities/test_video.py
@@ -4,15 +4,11 @@ Created on 8 May 2015
 @author: Mark
 '''
 from root.nested.config import config
-from selenium import webdriver
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 from PIL import Image
 import math
 import logging
-import sys
+import subprocess
 
 ALIFS = config.get_alifs()
 
@@ -25,23 +21,10 @@ class Capture():
     def run(self):
         logging.info("ADDER: Getting image")
         target = "http://%s" % ALIFS[self.alif]
-        if sys.platform == "win32":
-            path = "phantomjs.exe"
-        elif sys.platform == "linux2":
-            path = "/var/bin/phantomjs"
         try:
-            driver = webdriver.PhantomJS(executable_path=path)
-            wait = WebDriverWait(driver, 30)
-            driver.get(target)
-            button = By.CSS_SELECTOR, "#refresh_thumbnail"
-            wait.until(EC.presence_of_element_located(button)).click()
-            image = By.CSS_SELECTOR, "#right > div > a:nth-child(5)"
-            wait.until(EC.element_to_be_clickable(image)).click()
-            driver.save_screenshot("../imgs/capture.png")
+            subprocess.Popen("ruby capture.rb %s" % target)
         except TimeoutException as e:
             logging.info("ADDER: Timed out, stacktrace: %s" % e)
-        finally:
-            driver.quit()
 
 
 class ImageCompare():
