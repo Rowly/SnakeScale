822cb1789e3195507b14e170fb543aa12c904a09
Rowly <mail.rowlands@gmail.com>
2016-06-29 15:23:25 +0100

    Further decoding and debug prints

--- src/utilities/serial_controller.py
+++ src/utilities/serial_controller.py
@@ -9,14 +9,17 @@ import serial
 def connect(channel):
     channel = channel.encode("UTF-8")
     ser = serial.Serial("/dev/ttyS1", baudrate=1200)
-    print(ser.name)
-    ser.write(channel)
-    data = ser.read_all()
-    data = str(data)
-    assert len(data) < 1
-    assert data.startswith(channel.decode("UTF-8"))
-    assert data.endswith(channel.decode("UTF-8"))
-    ser.close()
+    try:
+        print(ser.name)
+        ser.write(channel)
+        data = ser.read_all()
+        data = data.decode("UTF-8")
+        print(data)
+        assert len(data) < 1
+        assert data.startswith(channel.decode("UTF-8"))
+        assert data.endswith(channel.decode("UTF-8"))
+    finally:
+        ser.close()
 
 if __name__ == "__main__":
     for each in ["1", "2", "3", "4"]:
