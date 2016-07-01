859a1afcae98b0b336ab6eaf7236a55491f09fe6
Rowly <mail.rowlands@gmail.com>
2016-07-01 16:41:06 +0100

    Very confused about the difference between win and linux paths. If any.

--- src/utilities/capture_gui.py
+++ src/utilities/capture_gui.py
@@ -38,12 +38,12 @@ class CaptureGui(QtGui.QMainWindow):
 
     def keyPressEvent(self, e):
         if e.key() == QtCore.Qt.Key_F1:
-            system = platform.system()
-            if system == "Win32" or system == "Windows":
-                path = "../dump/test.txt"
-            else:
-                path = "./dump/test.txt"
-            with open(path, "w+") as file:
+#             system = platform.system()
+#             if system == "Win32" or system == "Windows":
+#                 path = "../dump/test.txt"
+#             else:
+#                 path = "./dump/test.txt"
+            with open("./dump/test.txt", "w+") as file:
                 file.write(self.area.toPlainText())
             self.close()
 
